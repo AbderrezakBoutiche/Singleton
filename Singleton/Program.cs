21a5235e69c286e784f431631f2ad885ca27d6a5
cdi9bout <cdi9bout@M3245>
2017-10-11 15:05:15 +0200

    Ajoutez des fichiers projet.

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton thon = new Singleton();
+            //Singleton mayo = new Singleton();
+            Singleton salade = Singleton.CreerSingleton();
+            Singleton thon = Singleton.CreerSingleton();
+
+            Console.WriteLine(thon.Equals(salade));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton singleton; // On creer l'objet singleton en private pour qu'il ne soit utiliser que dans cette classe
+
+
+        public static Singleton CreerSingleton() // On met public pour pouvoir l'appeler ailleur
+        {
+            if (singleton == null) // On test si c'est null pour pouvoir l'instancier
+            {
+                singleton = new Singleton(); // On l'instancie
+                return singleton;
+            }
+            else
+            {
+                return singleton; // On retourne singleton car c'est toujours le même objet qu'on veut.
+            }
+            
+        }
+
+        private Singleton () // On creer une methode vide en private pour ne plus pouvoir l'instancier de cette manière "Singleton s1 = new Singleton()"
+        {
+
+        }
+    }
+}
